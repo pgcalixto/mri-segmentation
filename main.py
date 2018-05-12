67d11c0033ac8c08684d21a66ca4e24269feb137
Pedro Mendonça <pgcalixto@gmail.com>
2018-05-12 09:38:27 -0300

    Add initial Python program
    This program shows a 2D slice of the image read from a local MAT file
    and sets user key events (keys 'j' and 'k') and navigates backward and
    forward through slices in the X-Z plane, by increasing/decreasing the Y
    coordinate.

---
+++ main.py
@@ -0,0 +1,68 @@
+import scipy.io
+import matplotlib.pyplot as plt
+import numpy as np
+
+def load_mat_img():
+    """Read image data from local MAT file
+
+    Reads local 'vbm.mat' data and returns its array of values.
+
+    Returns:
+        numpy.ndarray: Array of values read from MAT file
+    """
+    # read mat image and normalize it
+    mat_data = scipy.io.loadmat('vbm.mat')
+    image = mat_data['vbm'] / np.amax(mat_data['vbm'])
+
+    return image
+
+def multi_slice_viewer(image):
+    """Display image and sets key press event.
+
+    Displays 3D input image data in 2D slice and sets the key press events which
+    go forward and backward through slices.
+
+    Args:
+        image (numpy.ndarray): tridimensional array of image values
+    """
+    # saves image in axis data, sets slice index and plots it
+    fig, axes = plt.subplots()
+    axes.image3d = image
+    axes.index = 1
+    axes.imshow(image[:][axes.index][:], vmin=0, vmax=1)
+    # sets the key event
+    fig.canvas.mpl_connect('key_press_event', process_key)
+    plt.show()
+
+def process_key(event):
+    fig = event.canvas.figure
+    axis = fig.axes[0]
+    if event.key == 'j':
+        previous_slice(axis)
+    elif event.key == 'k':
+        next_slice(axis)
+    fig.canvas.draw()
+
+def previous_slice(axis):
+    """Go to the previous slice."""
+    image3d = axis.image3d
+    axis.index = (axis.index - 1) % image3d.shape[1]  # wrap around using %
+    axis.images[0].set_array(image3d[:][axis.index][:])
+
+def next_slice(axis):
+    """Go to the next slice."""
+    image3d = axis.image3d
+    axis.index = (axis.index + 1) % image3d.shape[0]
+    axis.images[0].set_array(image3d[:][axis.index][:])
+
+
+def main():
+    """Main function.
+
+    This function is responsible for reading a MAT file and displaying its data.
+    """
+    img = load_mat_img()
+    multi_slice_viewer(img)
+
+if __name__ == '__main__':
+    main()
